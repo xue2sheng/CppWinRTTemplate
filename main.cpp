eeb6e9d39d95d236477e3cd2e033a33256a736eb
Andres Sevillano <asevillano@kx.com>
2020-10-26 00:42:49 +0000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿using namespace std;
+
+int main()
+{
+	cout << "Hello CMake..." << endl;
+	return 0;
+}
---
+++ pch.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include <iostream>
+#include <windows.ui.composition.interop.h>
+#include <ShellScalingAPI.h>
+#include <DispatcherQueue.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
